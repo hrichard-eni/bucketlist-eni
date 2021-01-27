8505e248bb0e3e8a2201f2c361dbf697ca6b2bb8
Hugo <hugo.richard2020@campus-eni.fr>
2021-01-27 11:12:47 +0100

    module 5, TP en cours...

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,5 +24,35 @@ class WishController extends AbstractController
     public function detail($id): Response
     {
         return $this->render('wish/detail.html.twig', ["id"=> $id]);
+    }
+
+    /**
+     * @Route("/test", name="wish_test")
+     */
+    public function test(EntityManagerInterface $entityManager)
+    {
+        $wish = new Wish();
+
+        $wish->setTitle('Apprendre le norvégien');
+        $wish->setDescription('Apprendre le norvégien, pour écrire des textes de Black Metal');
+        $wish->setAuthor('Hugo');
+        $wish->setIsPublished(true);
+        $wish->setDateCreated(new \DateTime());
+
+        $entityManager->persist($wish);
+
+        $wish2 = new Wish();
+
+        $wish2->setTitle('Gagner au loto');
+        $wish2->setDescription('Un jour, les bons numéros seront miens');
+        $wish2->setAuthor('Sigismond');
+        $wish2->setIsPublished(true);
+        $wish2->setDateCreated(new \DateTime());
+
+        $entityManager->persist($wish2);
+
+        $entityManager->flush();
+
+        return $this->render('wish/test.html.twig');
     }
 }
